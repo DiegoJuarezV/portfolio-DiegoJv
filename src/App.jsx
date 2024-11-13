cf96aec5f1104d5778995c54594eec27ce70ebe9
DiegoJuarezV <diegojve27@gmail.com>
2024-11-13 16:11:38 -0600

    subiendo proyecto a github

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-blue-700 text-center font-bold uppercase mt-8">
+        hola este es un proyecto de react para crear un portfolio
+      </h1>
+    </>
+  )
+}
+
+export default App
